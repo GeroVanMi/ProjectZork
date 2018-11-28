6c1860ffdee9f59bddbeaaeb73b103a2e93c88d2
GeroVanMi <gerome.meyer@icloud.com>
2018-11-28 15:56:11 +0100

    Created tests with Javadoc documentation for Backpack-class

--- src/test/java/ch/bbw/zork/BackpackTest.java
+++ src/test/java/ch/bbw/zork/BackpackTest.java
@@ -3,16 +3,46 @@ package ch.bbw.zork;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class BackpackTest {
     Backpack backpack;
 
+    /**
+     * Create the backpack.
+     */
     @Before
     public void setup() {
         backpack = new Backpack();
     }
 
+    /**
+     * Check if a single item can be added to the backpack.
+     */
     @Test
     public void testAdding() {
         backpack.addItem(new Item("Pickaxe", "It's a pickaxe"));
+        assertEquals("Expect one item to be in the inventory.", 1, backpack.getAmountOfItems());
+    }
+
+    /**
+     * Check if an item can only be added once.
+     */
+    @Test
+    public void addSameItem() {
+        Item pickaxe = new Item("Pickaxe", "It's a pickaxe");
+        backpack.addItem(pickaxe);
+        backpack.addItem(pickaxe);
+        assertEquals("Expect second pickaxe to be deleted.", 1, backpack.getAmountOfItems());
+    }
+
+    /**
+     * Check if multiple Items can be added to the backpack.
+     */
+    @Test
+    public void addMultipleItems() {
+        backpack.addItem(new Item("House", "A House"));
+        backpack.addItem(new Item("Brick", "A Brick"));
+        assertEquals("Expect 2 items to be in the backpack.", 2, backpack.getAmountOfItems());
     }
 }
