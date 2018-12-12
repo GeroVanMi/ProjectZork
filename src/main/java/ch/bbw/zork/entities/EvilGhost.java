a6d0c1c52a99fbdf66fbd6135406988e898f6fdf
TheCodingLab <nairolf.retlaw@gmail.com>
2018-12-12 13:37:11 +0100

    Added killing mechanic

--- src/main/java/ch/bbw/zork/entities/EvilGhost.java
+++ src/main/java/ch/bbw/zork/entities/EvilGhost.java
@@ -3,8 +3,10 @@ package ch.bbw.zork.entities;
 import ch.bbw.zork.Game;
 
 public class EvilGhost extends Ghost {
+    
     @Override
     public void respond(Game game) {
-        //Kill player
+        System.out.println("Buh!");
+        game.killPlayer();
     }
 }
